5f83ddf6c0d8e0264fe395512cf537b78d42415b
nbogie <neillbogie@googlemail.com>
2025-11-28 07:26:37 +0000

    midway

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import YahtzeeApp from "./YahtzeeApp";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="App">
+                <YahtzeeApp />
+            </div>
+        </>
+    );
+}
+
+export default App;
